ebd4c1dc23f5bd13e12ec0f75da5f19c5eb853af
yangdao <458424869@qq.com>
2022-01-10 16:59:52 +0800

    test macro

--- tests/core/color.glsl
+++ tests/core/color.glsl
@@ -2,10 +2,13 @@ use core.math;
 
 CustomType vs_main(float paramA,const float paramB,float cache) {
     float a = pow5(123);
-   
+    #ifdef HAS_POSITION
     for(int i = 0; i< 10;i++) {
         float c = i;
-    } 
+    }
+    #else
+      int no = 11;
+    #endif
   
     a = cPI;
 }
