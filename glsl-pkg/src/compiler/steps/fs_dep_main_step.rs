2bbbaef9b504967bec971ea361bb7581391069cc
yangdao <458424869@qq.com>
2022-05-02 21:25:47 +0800

    支持像素shader多返回值

--- glsl-pkg/src/compiler/steps/fs_dep_main_step.rs
+++ glsl-pkg/src/compiler/steps/fs_dep_main_step.rs
@@ -1,33 +1,120 @@
 use std::{fmt::Write, sync::Arc};
 
-use glsl_lang::{ast::*, transpiler::glsl::show_function_definition};
+use glsl_lang::{ast::*, transpiler::glsl::{show_function_definition}};
+
 
 use crate::{pkg_inst::PackageInstance, compiler::sym_generator::SymbolGenerator, ast::SymbolName};
 
 use super::ReplaceReturnStmt;
 
-pub fn run_fs_dep_main_step<F:FnOnce(&mut W),W:Write>(main_name:&str,inst:Arc<PackageInstance>,writer:&mut W,sym:Option<SymbolName>,f:F) {
+pub fn run_fs_dep_main_step<F:FnOnce(&mut W),W:Write>(main_name:&str,inst:Arc<PackageInstance>,writer:&mut W,input_sym:Option<SymbolName>,f:F) {
     let mut sym_gen = SymbolGenerator::new(inst.clone());
     let main_sym_name = SymbolName::parse(main_name);
     sym_gen.run(&main_sym_name,writer,false);
 
-    if let Some(in_sym) = sym.as_ref() {
+    if let Some(in_sym) = input_sym.as_ref() {
         writer.write_str("\r\n").unwrap();
         writer.write_fmt(format_args!("layout(location = 0) in {} _input;\r\n",&in_sym.name)).unwrap();
     }
     f(writer);
+
     if let Some((decl,_file)) = inst.ast_pkg.find_decl(&main_sym_name) {
         match &decl.content {
             ExternalDeclarationData::FunctionDefinition(fd) => {
-                let new_func = re_generator_function(fd,sym.as_ref());
-                writer.write_str("\r\n").unwrap();
-                show_function_definition(writer, &new_func, &mut sym_gen.fs).unwrap();
+                if let TypeSpecifierNonArrayData::TypeName(type_name)  = &fd.prototype.ty.ty.ty.content {
+                   let mut type_sym = main_sym_name.clone();
+                   type_sym.name = type_name.as_str().to_string();
+                   let find_type = inst.ast_pkg.find_decl(&type_sym);
+                   if let Some(struct_type) = find_type {
+                       re_generator_struct_function(writer,fd,struct_type.0,&mut sym_gen,input_sym.as_ref());
+                   } else {
+                       log::error!("not found type:{:?}",&type_sym);
+                   }
+                } else {
+                    writer.write_str("layout(location = 0) out vec4 _outColor;\r\n").unwrap();
+                    let new_func = re_generator_function(fd,input_sym.as_ref());
+                    writer.write_str("\r\n").unwrap();
+                    show_function_definition(writer, &new_func, &mut sym_gen.fs).unwrap();
+                }  
             },
             _ => {
                log::error!("{:?} must be function",main_name);
             }
          }   
     }
+}
+
+fn re_generator_struct_function<W:Write>(write:&mut W,old_func:&FunctionDefinition,
+                                         struct_decl:&ExternalDeclaration,sym_gen:&mut SymbolGenerator,
+                                         in_type_sym:Option<&SymbolName>) {
+    let (attrs,struct_name) = find_struct_attrs(struct_decl);
+    let mut index = 0;
+    for (typ,name) in attrs.iter() {
+        let type_str = match typ.content {
+            TypeSpecifierNonArrayData::Vec4 => {"vec4"},
+            TypeSpecifierNonArrayData::Vec3 => {"vec3"},
+            _ => {
+                log::error!("error fs output type:{:?}",typ);
+                return;
+            },
+        };
+        write.write_str(&format!("layout(location = {}) out {} _fs_output_{};\r\n",index,type_str,name.as_str())).unwrap();
+        index += 1;
+    }
+    
+    let mut rename_old_func = old_func.clone();
+    rename_old_func.content.prototype.name.0 = "_muli_main".into();
+    let in_param = rename_old_func.prototype.parameters[0].clone();
+    let in_param_name = match &in_param.content {
+        FunctionParameterDeclarationData::Named(_,namded) => {
+            namded.ident.ident.as_str().to_string()
+           
+        },
+        _ => { 
+            String::default()
+        },
+    };
+    let fst_decl = make_decl("_input", in_param_name.as_str(), &in_type_sym.unwrap().name);
+    rename_old_func.content.statement.content.statement_list.insert(0, fst_decl);
+
+    rename_old_func.prototype.parameters.clear();
+    show_function_definition(write, &rename_old_func, &mut sym_gen.fs).unwrap();
+   
+    
+    //show_type_specifier_non_array(write, &attrs[0].0, &mut sym_gen.fs);
+    //dbg!(&attrs);
+    write.write_str("void main() {\r\n").unwrap();
+    write.write_str(&format!("  {} value = _muli_main();\r\n",struct_name.as_str())).unwrap();
+    for (_,name) in attrs {
+        write.write_str(&format!("  _fs_output_{} = value.{};\r\n",name.as_str(),name.as_str())).unwrap();
+    }
+    write.write_str("\r\n}").unwrap();
+
+}
+
+fn find_struct_attrs(struct_decl:&ExternalDeclaration) -> (Vec<(TypeSpecifierNonArray,String)>,String) {
+    let mut attrs:Vec<(TypeSpecifierNonArray,String)> = vec![];
+    let mut type_name = String::default();
+    match &struct_decl.content {
+        ExternalDeclarationData::Declaration(decl) => {
+            match &decl.content {
+                DeclarationData::InitDeclaratorList(decl_list) => {
+                    if let TypeSpecifierNonArrayData::Struct(struct_type) = &decl_list.head.ty.ty.ty.content {
+                        type_name = struct_type.name.as_ref().unwrap().as_str().to_string();
+                        for filed in struct_type.fields.iter() {
+                            let ident_type = filed.ty.content.ty.content.clone();
+                            let ident_name = filed.identifiers[0].content.ident.as_str();
+                            attrs.push((ident_type.into(),ident_name.to_string()));
+                        }
+                    }
+                   
+                },
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+    (attrs,type_name)
 }
 
 fn find_fst_param_name(fd:&FunctionDefinition) -> Option<String> {
